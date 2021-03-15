c0e2e2ecc2f3526e8bc011252f55f136e5253d15
cchoongh <cndgh49@naver.com>
2021-03-15 17:45:25 +0900

    Configure initial setting

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: {
+    app: ['@babel/polyfill', __dirname + '/src/js/app.js'],
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+};
