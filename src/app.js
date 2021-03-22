416e3ad42a563dff5747a8f7b90a090d0ca86761
cchoongh <cndgh49@naver.com>
2021-03-22 12:52:40 +0900

    TMPTMPTMPTMPTMPTMPTMPTPMTPMTPTMP

--- src/app.js
+++ src/app.js
@@ -1,20 +1,43 @@
 import './style.scss';
-
 import { _ } from './util.js';
 import { generateConnectionLineData } from './ladder-board/core-util.js';
-import { LadderBoardView } from './ladder-board/LadderBoard.js';
+import { LadderBoardView } from './ladder-board/LadderBoardView.js';
+import { PagerView } from './pager/PagerView.js';
+import { PageView} from './pager/PageView.js';
 
-import { TestData } from './testData.js';
+document.addEventListener('DOMContentLoaded', pagerTest);
 
-document.addEventListener('DOMContentLoaded', () => {
-  const inputData = Array(4); // FOR TEST
-  const connectionLineData = TestData.data3;
+
+function ladderBoardViewTest() {
+
+
+  _.$('.test-cont').appendChild(ladderBoardView.getEl());
+}
+
+function pagerTest() {
+  const ladderBoardView = createLadderBoardView();
+
+  const pagerView = new PagerView({ width: 700, height: 450 });
+  pagerView.appendPageView({ $content: _.genEl('DIV', { classNames: ['page1'] })});
+  pagerView.appendPageView({ $content: ladderBoardView.getEl() });
+  pagerView.appendPageView({ $content: _.genEl('DIV', { classNames: ['page3'] })});
+  document.body.appendChild(pagerView.getEl());
+}
+
+function createLadderBoardView() {
+  const inputTexts = ['a', 'aaaa', 'aaaaaaaa', '4', '5', '6'];
+  const outputTexts = ['b', 'bbbb', 'bbbbbbbb', '4', '5', '6'];
+  const connectionLineData = generateConnectionLineData({
+    columnSize: inputTexts.length,
+  });
+
+  // console.log(connectionLineData);
 
   const ladderBoardView = new LadderBoardView({
-    inputData,
-    outputData: [],
+    inputTexts,
+    outputTexts,
     connectionLineData,
   });
 
-  _.$('.ladder-cont').appendChild(ladderBoardView.getEl());
-});
+  return ladderBoardView;
+}
---
+++ src/game-setting-view/GameSettingView.js
@@ -0,0 +1,13 @@
+import './style.scss';
+import { _ } from '../util.js';
+
+export class GameSettingView {
+  constructor() {
+    
+  }
+
+  template() {
+    return `<div class="input-cont"></div>
+            <div class="btn-cont"></div>`
+  }
+}
